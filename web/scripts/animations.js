2d901600805596aee59075c1ec8e5d65e9a82f65
David Danaher <david.danaher@uptake.com>
2016-02-29 08:10:48 -0600

    feat(home): animations - logo and text fade in

--- web/scripts/animations.js
+++ web/scripts/animations.js
@@ -2,30 +2,44 @@ var logo = document.getElementsByClassName('logo');
 var bandName = document.getElementsByClassName('banner');
 var back = document.getElementsByClassName('container-fluid');
 
+init(logo);
+init(bandName);
 fadeIn(logo, 6);
 fadeIn(bandName, 7);
-reSize(logo, 5);
-moveHeader();
-fade(back);
+reSizeLogo(logo, 5);
+reSizeName(bandName, 6);
+// moveHeader();
+// fade(back);
+
+function init(tag) {
+    TweenMax.set(tag, {opacity: 0});
+    playMusic();
+}
 
 function fadeIn(tag, time) {
     TweenMax.to(tag, time, {opacity: 1, ease:Power1.easeIn});
 }
 
-function reSize(tag, time) {
-  TweenMax.to(tag, time, {width: '400px', height: '350px', margin: '200px auto'});
+function reSizeLogo(tag, time) {
+  TweenMax.from(tag, time, {width: '1200px', height: '1100px'});
+}
+
+function reSizeName(tag, time) {
+    TweenMax.from(tag, time, {width: '1200px', height: '120px'})
 }
 
 function moveHeader() {
   TweenMax.to(bandName, 7, {height: '50px'});
 }
 
-function fade(tag) {
-  TweenMax.to(tag, 4, {color: rgba(0, 0, 0, 0.75), ease:Power2.easeIn});
-}
-
+// function fade(tag) {
+//   TweenMax.to(tag, 4, {color: rgba(0, 0, 0, 0.75), ease:Power2.easeIn});
+// }
 
 
 //http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
+
+function playMusic() {
 var audio = new Audio('../resources/sounds/intro3.mp3');
-// audio.play();
+audio.play();
+}
