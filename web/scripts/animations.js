afaa5cbe8980a27b2ccd98dd4250fcd8aa672423
David Danaher <david.danaher@uptake.com>
2016-03-03 08:22:51 -0600

    feat(home-animations): add second row for additional information

--- web/scripts/animations.js
+++ web/scripts/animations.js
@@ -5,50 +5,30 @@ var album = document.getElementsByClassName('album');
 var judge = document.getElementsByClassName('word-1');
 var is = document.getElementsByClassName('word-2');
 var coming = document.getElementsByClassName('word-3');
+var releaseDate = 'May 14, 2016';
 
 var intro = new TimelineMax();
 
 init();
-// fadeIn(logo, 6);
-// fadeIn(bandName, 7);
-// fadeIn(album, 9);
-// reSizeLogo(logo, 4);
-// reSizeName(bandName, 5);
-// text(album, 6);
 
-intro.add(TweenMax.to(logo, 8, {opacity: 1}),
-TweenMax.to(bandName, 8, {opacity:1, delay: 2}),
-TweenMax.to(judge, 3, {opacity: 1, delay: 5}),
-TweenMax.to(is, 3, {opacity: 1, delay: 7}),
-TweenMax.to(coming, 3, {opacity: 1, delay: 9}));
-// TweenMax.to(album, 3, {opacity: 1,text:{value:'Judgement is coming', delimiter:' '}, delay: 3, ease:Linear.easeNone}));
-
+intro.add(
+    TweenMax.to(logo, 6, {opacity: 1}),
+    TweenMax.to(bandName, 6, {opacity:1, delay: 2}),
+    TweenMax.to(judge, 3, {opacity: 1, delay: 4}),
+    TweenMax.to(is, 3, {opacity: 1, delay: 6}),
+    TweenMax.to(coming, 3, {opacity: 1, delay: 8}),
+    TweenMax.to(album, 2, {opacity:0, delay: 10}),
+    TweenMax.set(album, {text: releaseDate, delay: 12}),
+    TweenMax.to(album, 2, {opacity:1, delay:12})
+);
 
 function init() {
     TweenMax.set(logo, {opacity: 0});
     TweenMax.set(bandName, {opacity: 0});
-    // TweenMax.set(album, {text: '', opacity: 0.4});
     TweenMax.set(judge, {opacity:0});
     TweenMax.set(is, {opacity:0});
     TweenMax.set(coming, {opacity:0});
-    playMusic();
-}
-
-function fadeIn(tag, time, delay) {
-    TweenMax.to(tag, time, {opacity: 1, delay: delay, ease:Power1.easeOut});
-}
-
-
-function reSizeLogo(time) {
-  TweenMax.from(logo, time, {width: '1200px', height: '1100px', ease:Power1.easeOut});
-}
-
-function reSizeName(tag, time) {
-    TweenMax.from(tag, time, {width: '1200px', height: '120px', ease:Power1.easeOut});
-}
-
-function text(tag, time) {
-    TweenMax.to(tag, time, {text:'Judgement is coming', ease:Linear.easeNone});
+    // playMusic();
 }
 
 //http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
