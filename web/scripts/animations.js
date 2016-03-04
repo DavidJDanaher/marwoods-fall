ccf8bb8fe642ee8503b858e9427330ef2a69a8fc
David Danaher <david.danaher@uptake.com>
2016-03-04 07:25:34 -0600

    feat(home-animations): timeline nearly complete

--- web/scripts/animations.js
+++ web/scripts/animations.js
@@ -1,26 +1,34 @@
+var row1 = document.getElementsByClassName('row-1');
 var logo = document.getElementsByClassName('logo-img');
 var bandName = document.getElementsByClassName('band-name');
-var back = document.getElementsByClassName('container-fluid');
 var album = document.getElementsByClassName('album');
 var judge = document.getElementsByClassName('word-1');
 var is = document.getElementsByClassName('word-2');
 var coming = document.getElementsByClassName('word-3');
-var releaseDate = 'May 14, 2016';
+var release = document.getElementsByClassName('release');
+var row2 = document.getElementsByClassName('row-2');
+var footer = document.getElementsByClassName('footer');
 
-var intro = new TimelineMax();
+var tL = new TimelineMax();
+var t = 0;
 
 init();
 
-intro.add(
-    TweenMax.to(logo, 6, {opacity: 1}),
-    TweenMax.to(bandName, 6, {opacity:1, delay: 2}),
-    TweenMax.to(judge, 3, {opacity: 1, delay: 4}),
-    TweenMax.to(is, 3, {opacity: 1, delay: 6}),
-    TweenMax.to(coming, 3, {opacity: 1, delay: 8}),
-    TweenMax.to(album, 2, {opacity:0, delay: 10}),
-    TweenMax.set(album, {text: releaseDate, delay: 12}),
-    TweenMax.to(album, 2, {opacity:1, delay:12})
-);
+tL.to(logo, 5, {opacity: 1, ease:Power1.easeIn}, t)
+    .to(bandName, 4, {opacity:1, ease:Power1.easeIn}, t+=3)
+    .to(judge, 2, {opacity: 1}, t+=4)
+    .to(is, 2, {opacity: 1}, t+=1)
+    .to(coming, 2, {opacity: 1}, t+=1)
+    .to(album, 1, {opacity:0}, t+=1)
+    .set(album, {height: 0}, t+=1)
+    .set(release, {height: 'auto'}, t)
+    .to(release, 2, {opacity:1}, t)
+    .to(release, 1, {opacity:0}, t+=2)
+    .set(row1, {height: '600px'}, t+=1)
+    .set(row2, {visibility: 'visible'}, t);
+
+
+
 
 function init() {
     TweenMax.set(logo, {opacity: 0});
@@ -28,7 +36,9 @@ function init() {
     TweenMax.set(judge, {opacity:0});
     TweenMax.set(is, {opacity:0});
     TweenMax.set(coming, {opacity:0});
-    // playMusic();
+    TweenMax.set(row2, {visibility: 'hidden'});
+    TweenMax.set(release, {height: 0, opacity: 0});
+    playMusic();
 }
 
 //http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
