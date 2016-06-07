238fb4361ed689398550e1db30b2278856fa9726
David Danaher <david.danaher@uptake.com>
2016-06-07 18:01:18 -0500

    Abstract navigation and footer html into component files for AJAX inclusion

---
+++ web/scripts/components.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.navbar-default').load('http://localhost:8000/web/components/nav.html'); //change to marwood's domain
+});
+
+$(document).ready(function() {
+    $('.footer').load('http://localhost:8000/web/components/footer.html'); //change to marwood's domain
+});
