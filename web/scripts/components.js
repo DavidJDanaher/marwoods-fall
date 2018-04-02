1c545f0df01f55f25ee7de0f92ecf4c5624eeeec
David Danaher <ddanaher@gmail.com>
2018-04-01 21:38:01 -0500

    footer fix

--- web/scripts/components.js
+++ web/scripts/components.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $('.navbar-default').load('http://marwoodsfall.com/web/components/nav.html'); 
+    $('.navbar-default').load('http://marwoodsfall.com/web/components/nav.html');
 });
 
 $(document).ready(function() {
     $('.footer').load('http://marwoodsfall.com/web/components/footer.html');
+
+    var date = new Date()
+    $('.copy-year').text(date.getFullYear())
 });
